c9856b30d0d0d6ef66d2ee2c5a00a19d4ebf63a7
Yongsuk <84030256+MAOK-Yongsuk@users.noreply.github.com>
2022-12-15 00:02:40 +0700

    update twitter Sentiment Analysis

--- Web-scraping-Sentiment-Analysis/twitter-v2.py
+++ Web-scraping-Sentiment-Analysis/twitter-v2.py
@@ -9,13 +9,15 @@ import csv
 import urllib.request
 import PySimpleGUI as sg
 
+numTW = 20
+
 def twitter(name):
     # Created a list to append all tweet attributes(data)
     attributes_container = []
 
     # Using TwitterSearchScraper to scrape data and append tweets to list
     for i,tweet in enumerate(sntwitter.TwitterSearchScraper(f"from:{name}").get_items()):
-        if i>10:
+        if i>numTW:
             break
         attributes_container.append([tweet.date, tweet.likeCount, tweet.sourceLabel, tweet.content])
         
@@ -27,7 +29,7 @@ def twitter(name):
 def preprocess(text):
     new_text = []
  
-    for t in text.split(" ")
+    for t in text.split(" "):
         t = '' if t.startswith('@') and len(t) > 1 else t
         new_text.append(t)
     return " ".join(new_text)
@@ -36,7 +38,9 @@ sg.theme('DarkTeal12')
 backgroundColor = '#c8d2dc'
 textColor = '#1c587a'
 colorlist = ['green','yellow','red']
-
+listTW = []
+headings = ['      Tweets      ', 'positive', 'neutral', 'negative']
+col_widths = list(map(lambda x:len(x)+2, headings))
 
 tab1_layout = [  
             [sg.Image(filename='saiglogo.png',background_color=backgroundColor)],
@@ -52,12 +56,29 @@ tab1_layout = [
             [sg.Text('',background_color=backgroundColor, size=(0,3))]
         ]
 
-tab2_layout = [[sg.T('This is inside tab 2')],
-               [sg.In(key='in')]]
+tab2_layout = [
+            [sg.T('Twitter Name',size=(0,1),background_color=backgroundColor,text_color=textColor,font=('Courier',20,'bold'))],
+            [sg.In(key='-NAME-', font=('Courier 12')), sg.Button('COMPUTE', font=('Courier 15'),button_color = textColor, key='-COMPUTE2-')],
+            [sg.Table(values=listTW, headings=headings, max_col_width=20,
+                    auto_size_columns=True,
+                    justification='right',
+                    num_rows=numTW,
+                    key='-TABLE-',
+                    enable_events=True,
+                    text_color='#1c587a',
+                    alternating_row_color='grey90',
+                    # col_widths = col_widths,
+                    expand_x=True,
+                    expand_y=True,
+                    enable_click_events=True,
+                    background_color=backgroundColor)]
+            ]
+
 
 layout = [[sg.TabGroup([
-            [ sg.Tab('Tab 1', tab1_layout,background_color=backgroundColor,element_justification='c'), 
-              sg.Tab('Tab 2', tab2_layout)]])]]
+            [ sg.Tab('Manual', tab1_layout,background_color=backgroundColor,element_justification='c'), 
+              sg.Tab('Twitter', tab2_layout,background_color=backgroundColor,element_justification='c')
+              ]])]]
 
 window = sg.Window('Sentiment-Analysis',background_color=backgroundColor, layout = layout, size=(600,600), element_justification='c')
 
@@ -78,23 +99,7 @@ labels = [row[1] for row in csvreader if len(row) > 1]
 # PT
 model = AutoModelForSequenceClassification.from_pretrained(MODEL)
 model.save_pretrained(MODEL)
-tokenizer.save_pretrained(MODEL)
-
-
-
-listTW = []
-for i in range(5):
-    textTW = twitter("elonmusk")  
-    text = textTW[i]
-    text = preprocess(text)
-    # print(text)
-    encoded_input = tokenizer(text, return_tensors='pt')
-    output = model(**encoded_input)
-    scores = output[0][0].detach().numpy()
-    scores = softmax(scores)
-    listTW.append([text,scores[2], scores[1], scores[0]])
-
-print(listTW)
+# tokenizer.save_pretrained(MODEL)
 
 #---------------------------------------------------------------
 
@@ -112,7 +117,7 @@ while True:
         scores = output[0][0].detach().numpy()
         scores = softmax(scores)
         ranking = np.argsort(scores)
-        ranking = ranking[::-1]  
+        ranking = ranking[::-1]      
 
         for i in range(scores.shape[0]):
             l = labels[ranking[i]]
@@ -123,6 +128,20 @@ while True:
             elif(l == 'neutral'):
                 window[f'-OUTPUT{i+1}-'].update(f"{l} {np.round(float(s), 4)}", text_color=colorlist[1],background_color=backgroundColor)
             elif(l == 'negative'):
-                window[f'-OUTPUT{i+1}-'].update(f"{l} {np.round(float(s), 4)}", text_color=colorlist[2],background_color=backgroundColor)                  
+                window[f'-OUTPUT{i+1}-'].update(f"{l} {np.round(float(s), 4)}", text_color=colorlist[2],background_color=backgroundColor) 
+
+
+    if event == '-COMPUTE2-':
+        textTW = twitter(values['-NAME-'])  
+        for i in range(numTW):           
+            text = textTW[i]
+            text = preprocess(text)
+            encoded_input = tokenizer(text, return_tensors='pt')
+            output = model(**encoded_input)
+            scores = output[0][0].detach().numpy()
+            scores = softmax(scores)
+            listTW.append([text,scores[2], scores[1], scores[0]])
+        window['-TABLE-'].update(values=listTW)
+                                     
 
 window.close()   
